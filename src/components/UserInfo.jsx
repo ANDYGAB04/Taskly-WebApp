5f1b1399ceea3721dd2aa365629029c24e6d1d37
Suhan Mihai-Andrei <suhan.mihai.r4q@student.ucv.ro>
2025-04-22 14:00:18 +0300

    Dashboard

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Popover } from '@headlessui/react'
+import { get } from 'react-hook-form'
+import { getInitials } from '../utilities'
+import { Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+
+
+const UserInfo = ({user}) => {
+  return (
+    <div className='px-4'>
+        <Popover className='relative'>
+           {({open})=>(
+               <>
+               <Popover.Button className="group inline-flex items-center outline-none">
+                <span>
+                    {
+                        getInitials(user?.name)
+                    }
+                </span>
+               </Popover.Button>
+               <Transition
+                   as={Fragment}
+                   enter='transition ease-out duration-100'
+                   enterFrom=' opacity-0 scale-95'
+                   enterTo=' opacity-100 scale-100'
+                   leave='transition ease-in duration-75'
+                   leaveFrom='opacity-100 scale-100'
+                   leaveTo='opacity-0 scale-95'
+                   >
+                <Popover.Panel className='absolute left-1/2 z-10 mt-3 w-90 max-w-sm -translate-x-1/2 transform px-4 sm:px-0'>
+                   <div className='flex items-center gap-4 rounded-lg shadow-lg bg-white p-4'>
+                     <div className='w-16 h-16 rounded-full bg-fuchsia-700 text-white flex items-center justify-center text-2xl'>
+                        <span>
+                            {
+                                getInitials(user?.name)
+                            }
+                        </span>
+
+                     </div>
+                     <div className='flex flex-col gap-y-1'>
+                        <p className='text-black text-xl font-bold'> {user?.name}  </p>
+                          <span className='text-base text-gray-500 '>{user.title}</span>
+                          <span className='text-base text-blue-500 '>{user.email}</span>
+                     </div>
+
+                  </div>
+                </Popover.Panel>
+
+               </Transition>
+               
+               </>
+            )}
+        </Popover>
+
+    </div>
+  )
+}
+
+export default UserInfo
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,9 +1,45 @@
 export function getInitials(fullName) {
   const names = fullName.split(" ");
-
   const initials = names.slice(0, 2).map((name) => name[0].toUpperCase());
-
   const initialsStr = initials.join("");
-
   return initialsStr;
+}
+
+export const PRIOTITYSTYELS = {
+  high: "text-red-700",
+  medium: "text-yellow-500",
+  low: "text-pink-600",
+};
+
+export const TASK_TYPE = {
+  todo: "bg-red-700",
+  "in progress": "bg-purple-800",
+  completed: "bg-green-800",
+};
+
+export const BGS = [
+  "bg-fuchsia-800",
+  "bg-purple-700",
+  "bg-pink-600",
+  "bg-rose-500",
+];
+
+export const formatDate = (date) => {
+  const month = date.toLocaleString("en-US", { month: "short" });
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const formattedDate = `${day}-${month}-${year}`;
+  return formattedDate;
+};
+
+export function dateFormatter(dateString) {
+  const inputDate = new Date(dateString);
+  if (isNaN(inputDate)) {
+    return "Invalid Date";
+  }
+  const year = inputDate.getFullYear();
+  const month = String(inputDate.getMonth() + 1).padStart(2, "0");
+  const day = String(inputDate.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
 }
